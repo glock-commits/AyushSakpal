f9dc955325c8d2b4636ab8f9bf8ceca2797bff74
Glock <mcat42159@gmail.com>
2026-08-17 15:30:41 +0530

    Initial

---
+++ style and script/script.js
@@ -0,0 +1,30 @@
+const observer = new IntersectionObserver ((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            console.log(entry.target)
+            entry.target.classList.add("show")
+        }else{
+            entry.target.classList.remove("show")
+        };
+    });
+});
+const elementsToObserve = document.querySelectorAll(".para1, .card-container, .para2, #card1, #card2"
+);
+elementsToObserve.forEach(el => observer.observe(el));
+
+const navButtons = document.querySelectorAll('.navbar button');
+
+navButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const targetId = button.id;
+    const targetClass = targetId === 'skills' ? '.skill-container' : `.${targetId}`;
+    const targetSection = document.querySelector(targetClass);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
